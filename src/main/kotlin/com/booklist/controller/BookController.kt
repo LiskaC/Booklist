23a7920be3714233b254b09f2f1fd7e2c7ce4e9b
Liska Crofts <38409100+LiskaC@users.noreply.github.com>
2024-11-29 19:06:27 +0100

    Extended api with endpoint to fetch book by name
    The endpoint just returns the book name to begin with. This is of
    course useless (although it does tell us if the book is in the db
    or not!). This is a first step, next extra data will be added to
    the books table.

--- src/main/kotlin/com/booklist/controller/BookController.kt
+++ src/main/kotlin/com/booklist/controller/BookController.kt
@@ -1,14 +1,26 @@
 package com.booklist.controller
 
 import com.booklist.repository.BookRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class BookController(private val bookRepository: BookRepository) {
 
   @GetMapping("/books")
-    fun getBooks(): List<String> {
-      return bookRepository.getBooks()
+    fun getAllBooks(): List<String> {
+      return bookRepository.getAllBooks()
+    }
+
+  @GetMapping("/book")
+    fun getBookByName(@RequestParam(name = "name") name: String): ResponseEntity<String> {
+      val book = bookRepository.getBookByName(name)
+      return if (book != null) {
+        ResponseEntity.ok(book)
+      } else {
+        ResponseEntity.notFound().build()
+      }
     }
 }
--- src/main/kotlin/com/booklist/repository/BookRepository.kt
+++ src/main/kotlin/com/booklist/repository/BookRepository.kt
@@ -7,11 +7,19 @@ import org.springframework.stereotype.Repository
 @Repository
 class BookRepository(private val dsl: DSLContext) {
 
-  fun getBooks(): MutableList<String> {
-    val table = DSL.table("book")
-    val nameField = DSL.field("name", String::class.java)
+  val table = DSL.table("book")
+  val nameField = DSL.field("name", String::class.java)
+
+  fun getAllBooks(): MutableList<String> {
     return dsl.select(nameField)
       .from(table)
       .fetch(nameField)
+  }
+
+  fun getBookByName(name: String): String? {
+    return dsl.select(nameField)
+      .from(table)
+      .where(nameField.eq(name))
+      .fetchOne(nameField)
   }
 }
